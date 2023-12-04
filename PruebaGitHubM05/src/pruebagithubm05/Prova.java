edae8009cde8198ab564fa1e0deddd3c45dcfc47
rosi2 <rosi2@ROSI>
2023-12-04 16:32:22 +0100

    afegit hello world!! doble

---
+++ PruebaGitHubM05/src/pruebagithubm05/Prova.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebagithubm05;
+
+/**
+ *
+ * @author rosi2
+ */
+public class Prova {
+    
+}
